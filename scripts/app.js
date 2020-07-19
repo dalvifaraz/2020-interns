3a31cb7cae578784a8bff860e2177b7a1f44456f
Faraz Dalvi <dalvifaraz@gmail.com>
2020-07-19 23:28:07 +0530

    Task 2 Completed

--- scripts/app.js
+++ scripts/app.js
@@ -6,14 +6,24 @@ const getData = async(url) => {
     return data;
 }
 
-const createChart = function(chartData) {
-    console.log(chartData);
+const createChart = function(chartData1,chartData2) {//chartData1,chartData2
+    //console.log(chartData);
+    console.log(chartData1)
+    console.log(chartData2)
     labelArray = new Array();
-    dataArray = new Array();
-    chartData.forEach(function(item){
-        labelArray.push(item['date']);
-        dataArray.push(item['rate']);
+    //dataArray = new Array();
+    INRArray = new Array();
+    GBPArray = new Array();
+
+    chartData1.forEach(function(item){
+    labelArray.push(item['date']);
+    INRArray.push(item['rate']);
     });
+
+    chartData2.forEach(function(item){
+    GBPArray.push(item['rate']);
+    });
+
     var ctx = document.getElementById('myChart').getContext('2d');
     var chart = new Chart(ctx, {
         // The type of chart we want to create
@@ -22,12 +32,22 @@ const createChart = function(chartData) {
         // The data for our dataset
         data: {
             labels: labelArray,
-            datasets: [{
-                label: 'INR to EUR rates',
-                backgroundColor: null,
-                borderColor: 'rgb(255, 99, 132)',
-                data: dataArray
-            }]
+            datasets: [
+                {
+                    label: 'INR to EUR rates',
+                    backgroundColor: null,
+                    borderColor: 'rgb(0, 255, 255)',
+                    //data: dataArray
+                    data: INRArray
+                },
+                {
+                    label: 'GBP to EUR rates',
+                    backgroundColor: null,
+                    borderColor: 'rgb(255, 99, 132)',
+                    //data: dataArray
+                    data: GBPArray
+                }
+            ]
         },
 
         // Configuration options go here
@@ -40,20 +60,25 @@ const createChart = function(chartData) {
 const parseData = function(data) {
     const dates = data.rates;
     let count = 0
-    let chartData = new Array();
+    //let chartData = new Array();
+    let chartData1 = new Array();
+    let chartData2 = new Array();
     for(let i = 1; i <=31; i++ ) {
         formattedDay = String(i).padStart(2, '0')
         let day = "2019-01-"+formattedDay;
         let date = dates[day];
         if(isValid(date)){
-            chartData.push({date:day, rate:date["INR"]});
+            //chartData.push({date:day, rate:date["INR"]});
+            chartData1.push({date:day, rate:date["INR"]});
+            chartData2.push({date:day, rate:date["GBP"]});
 //            console.log(date["INR"]);
             count = count + 1;
-//            console.log(date["INR"]);
+            //console.log(date["GBP"]);
         }
     }
 //        console.log(chartData);
-    createChart(chartData);
+    //createChart(chartData);
+    createChart(chartData1,chartData2);
 }
 
 
