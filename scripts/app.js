e167ada50723cd0b40af4154803a5004e52d786e
Faraz Dalvi <dalvifaraz@gmail.com>
2020-07-20 16:56:46 +0530

    Features 1 and 2 Implemented

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,28 @@
-var latestrateINR = "";
-var latestrateGBP = "";
+var currency = "INR";
+
+document.getElementById('submit_btn').addEventListener('click', function(){
+    var start_date = document.getElementById('start_date').value;
+    var end_date = document.getElementById('end_date').value;
+    currency = document.getElementById('currency').value;
+    if(start_date == ""){
+        start_date = "2020-06-01";
+    }
+    if(end_date == ""){
+        end_date = "2020-07-01";
+    }
+
+    console.log(start_date);
+    console.log(end_date);
+    console.log(currency);
+
+    setTimeout(function(){
+            getData('https://api.exchangeratesapi.io/history?start_at='+ start_date +'&end_at='+ end_date)
+                .then(data => parseData(data))
+                .catch(err => console.log(err));
+     }, 30);
+
+});
+
 const getData = async(url) => {
     const response = await fetch(url);
     if(!response.ok)
@@ -8,21 +31,16 @@ const getData = async(url) => {
     return data;
 }
 
-const createChart = function(chartData1,chartData2) {
+const createChart = function(chartData1) {
     console.log(chartData1)
-    console.log(chartData2)
     labelArray = new Array();
     INRArray = new Array();
-    GBPArray = new Array();
 
     chartData1.forEach(function(item){
     labelArray.push(item['date']);
     INRArray.push(item['rate']);
     });
 
-    chartData2.forEach(function(item){
-    GBPArray.push(item['rate']);
-    });
 
     var ctx = document.getElementById('myChart').getContext('2d');
     var chart = new Chart(ctx, {
@@ -34,14 +52,9 @@ const createChart = function(chartData1,chartData2) {
             labels: labelArray,
             datasets: [
                 {
-                    label: 'INR (current: ' + latestrateINR + ')',
+                    label: currency,
                     borderColor: 'rgb(0, 255, 255)',
                     data: INRArray
-                },
-                {
-                    label: 'GBP (current: ' + latestrateGBP + ')',
-                    borderColor: 'rgb(255, 99, 132)',
-                    data: GBPArray
                 }
             ]
         },
@@ -73,26 +86,14 @@ const parseData = function(data) {
     const dates = Object.entries(data.rates);
     let count = 0
     let chartData1 = new Array();
-    let chartData2 = new Array();
     dates.forEach(function(date){
-        console.log(date[1].INR);
-            chartData1.push({date:date[0], rate:date[1].INR});
-            chartData2.push({date:date[0], rate:date[1].GBP});
+        console.log(date[1][currency]);
+            chartData1.push({date:date[0], rate:date[1][currency]});
     });
-    createChart(chartData1,chartData2);
-}
-const setData = function(latestdata) {
-    const latestrate = latestdata.rates;
-    //console.log(latestrate['INR'])
-    latestrateINR = String(latestrate['INR']);
-    //console.log(latestrate['GBP'])
-    latestrateGBP = String(latestrate['CAD']);
+    createChart(chartData1);
 }
 
-getData('https://api.exchangeratesapi.io/latest')
-    .then(latestdata => setData(latestdata))
-    .catch(error => console.log(error));
 
-getData('https://api.exchangeratesapi.io/history?start_at=2020-07-01&end_at=2020-07-20')
+getData('https://api.exchangeratesapi.io/history?start_at=2020-06-01&end_at=2020-07-20')
     .then(data => parseData(data))
     .catch(err => console.log(err));
